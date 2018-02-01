986298442fbdc85ddccbd00fb9bbb705402645b6
Mantas Baltakis <mantas.baltakis@gmail.com>
2018-02-01 17:26:58 +0000

    0.1v
    Core functionality

---
+++ circle.js
@@ -0,0 +1,12 @@
+var i = 0;
+var y = 0;
+var x = 0;
+
+for (i = 0; i < 10; i++) {
+    x + 20;
+    y + 20;
+    function createCircle(x, y) {
+            var myCircle = new Path.Circle(new Point(x, y), 20);
+            myCircle.fillColor = 'purple';
+        }
+}
